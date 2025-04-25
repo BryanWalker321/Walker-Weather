504d2d017bb0b405322704d1fbafad0d1f537b33
BryanWalker321 <bryanwalker.gta1@gmail.com>
2025-04-25 19:28:49 +0100

    Add fxmanifest.lua
    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Control and randomise weather seamlessly in your GTA FiveM server.'
+author 'Bryan Walker'
+version '1.0.0'
+
+server_script 'server.lua'
